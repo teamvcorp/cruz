77de783a1e783ab8ab4b445d97effae9473f08db
Robert Von Der Becke <teamvcorp@thevacorp.com>
2023-07-16 18:32:13 -0500

    added media q

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import {
   BsFillTelephoneFill,
   BsFillEnvelopeFill,
@@ -15,207 +15,213 @@ import { PiPlantBold } from "react-icons/pi";
 export default function Home() {
   return (
     <>
-      <header className={styles.headerBody}>
-          
+      <header className={styles.headerImage}>
+        <Image
+          className={styles.image}
+          src="/headerwhite.png"
+          objectFit="contain"
+          layout="fill"
+        />
+      </header>
+      {/* <header className={styles.headerBody}>
         <div className={styles.headerLeft}>
           <span>
-            <Image src="/cruz.svg"  layout='fill' quality="80" />
+            <Image src="/cruz.svg" layout="fill" quality="80" />
           </span>
           <h2 className="whiteTxt">Licensed & Insured</h2>
         </div>
 
-
         <div className={styles.headerRight}>
           <span>
-            <Image src="/electric.svg" layout='fill' quality="80" />
+            <Image src="/electric.svg" layout="fill" quality="80" />
           </span>
           <h2 className="redTxt">Serving Storm Lake and Surrounding Areas</h2>
         </div>
+      </header> */}
 
-      </header>
-
-    <main className={styles.main}>
-      <div className={styles.left}>
-      
-
-        <div className={styles.contact}>
-          {/* <p className="temp">under construction</p> */}
-          <h4 className="h4bigger">
-            {" "}
-            <span>
-              <BsFillTelephoneFill color="white" size={25} />
-            </span>{" "}
-            (712) 732-0000{" "}
-          </h4>
-          <h4 className="h4bigger">
-            {" "}
-            <span>
-              <BsFillTelephoneFill color="white" size={25} />
-            </span>{" "}
-            (712) 299-7004{" "}
-          </h4>
-          <h4 className="h4bigger">
-            {" "}
-            <span>
-              <BsFillEnvelopeFill size={20} />
-            </span>{" "}
-            CruzElectric712@gmail.com{" "}
-          </h4>
-          <h4 className="h4bigger">
-            {" "}
-            <span>
-              <MdLocationOn />
-            </span>{" "}
-            222 West Milwaukee Ave. <br></br>&nbsp; &nbsp;Storm Lake, IA 50588{" "}
-          </h4>
-
-          <div>
-            <iframe
-              width="400"
-              height="250"
-              frameborder="0"
-              src="https://www.bing.com/maps/embed?h=300&w=400&cp=42.647167~-92.40875244140625&lvl=11&typ=d&sty=r&src=SHELL&FORM=MBEDV8"
-              scrolling="no"
-            ></iframe>
-            <div>
-              <h4>Hours</h4>
-              <p className="whiteTxt"> Monday-Friday 9:00am-5:00pm</p>
-            </div>
-            {/* <BingMap /> */}
-          </div>
-        </div>
-              <div className={styles.emergency}>
-                <h2>24hr Emergency Services Available!</h2>
-              </div>
-      </div>
-
-      <div className={styles.right}>
-        <div className={styles.rightTop}>
-          <div className={styles.aboutContainer}>
-            <h1>about us</h1>
-            <p>
+      <main className={styles.main}>
+        <div className={styles.left}>
+          <div className={styles.contact}>
+            {/* <p className="temp">under construction</p> */}
+            <h4 className="h4bigger">
               {" "}
-              Cruz Electric opened in 2020 in Cherokee Iowa. We are expanding to
-              our new location in Storm Lake Iowa and all surrounding areas. We
-              are here to serve the customer and provide excellent service. Cruz
-              Electric will cruise right through it!{" "}
-            </p>
-            <button> request a quote </button>
-          </div>
+              <span>
+                <BsFillTelephoneFill color="#003770" size={25} />
+              </span>{" "}
+              (712) 732-0000{" "}
+            </h4>
+            <h4 className="h4bigger">
+              {" "}
+              <span>
+                <BsFillTelephoneFill color="#003770" size={25} />
+              </span>{" "}
+              (712) 299-7004{" "}
+            </h4>
+            <h4 className="h4bigger">
+              {" "}
+              <span>
+                <BsFillEnvelopeFill size={20} />
+              </span>{" "}
+              CruzElectric712@gmail.com{" "}
+            </h4>
+            <div className="">
+              <iframe
+                width="350"
+                height="200"
+                frameborder="0"
+                src="https://www.bing.com/maps/embed?h=300&w=400&cp=42.647167~-92.40875244140625&lvl=11&typ=d&sty=r&src=SHELL&FORM=MBEDV8"
+                scrolling="no"
+              ></iframe>
+              <div>
+                <h4 className="h4bigger">
+                  {" "}
+                  <span>
+                    <MdLocationOn />
+                  </span>{" "}
+                  222 West Milwaukee Ave. <br></br>&nbsp; &nbsp;Storm Lake, IA
+                  50588{" "}
+                </h4>
 
-          <div className={styles.servicesContainer}>
-            <h1>services</h1>
-            <h3 className="darkBlue">
-              <span>
-                <MdHome />
-              </span>{" "}
-              residential
-            </h3>
-            <h3 className="yellow">
-              <span>
-                <BsFillBuildingsFill />
-              </span>{" "}
-              commercial
-            </h3>
-            <h3 className="blue">
-              <span>
-                <PiPlantBold />
-              </span>{" "}
-              agricultural
-            </h3>
-            <h3 className="red">
-              <span>
-                <MdPhonelink />
-              </span>{" "}
-              communications
-            </h3>
-            <h3 className="blueGrey">
-              <span>
-                <GiPowerGenerator />
-              </span>{" "}
-              generator install
-            </h3>
-            <h3 className="black">
-              <span>
-                <BiCameraHome />
-              </span>{" "}
-              security cameras
-            </h3>
+                <h4>Hours</h4>
+                <p className="darkBlueTxt"> Monday-Friday 9:00am-5:00pm</p>
+              </div>
+
+              {/* <BingMap /> */}
+            </div>
+          </div>
+          <div className={styles.emergency}>
+            <h2>24hr Emergency Services Available!</h2>
           </div>
         </div>
 
-        <div className={styles.testimonialsContainer}>
-          <section>
-          <h3 className="darkBlueTxt">Testimonials</h3>
-  
-
-            <div className="white">
-              <p className="darkBlueTxt">
-                Cruz fixed my electrical stuff quickly and efficiently!{" "}
-                <br></br>
-                -Bob
-              </p>
-              <span>
+        <div className={styles.right}>
+          <div className={styles.rightTop}>
+            <div className={styles.aboutContainer}>
+              <h1>about us</h1>
+              <p>
                 {" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-              </span>
+                Cruz Electric opened in 2020 in Cherokee Iowa. We are expanding
+                to our new location in Storm Lake Iowa and all surrounding
+                areas. We are here to serve the customer and provide excellent
+                service. Cruz Electric will cruise right through it!{" "}
+              </p>
+              <button> request a quote </button>
             </div>
 
-            <div className="white">
-              <p className="darkBlueTxt">
-                Cruz fixed my electrical stuff quickly and efficiently!{" "}
-                <br></br>
-                -Bob
-              </p>
-              <span>
-                {" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-              </span>
+            <div className={styles.servicesContainer}>
+              <h1>services</h1>
+              <h3 className="darkBlue">
+                <span>
+                  <MdHome />
+                </span>{" "}
+                residential
+              </h3>
+              <h3 className="yellow">
+                <span>
+                  <BsFillBuildingsFill />
+                </span>{" "}
+                commercial
+              </h3>
+              <h3 className="blue">
+                <span>
+                  <PiPlantBold />
+                </span>{" "}
+                agricultural
+              </h3>
+              <h3 className="red">
+                <span>
+                  <MdPhonelink />
+                </span>{" "}
+                communications
+              </h3>
+              <h3 className="blueGrey">
+                <span>
+                  <GiPowerGenerator />
+                </span>{" "}
+                generator install
+              </h3>
+              <h3 className="black">
+                <span>
+                  <BiCameraHome />
+                </span>{" "}
+                security cameras
+              </h3>
             </div>
-            <div className="white">
-              <p className="darkBlueTxt">
-                Cruz fixed my electrical stuff quickly and efficiently!{" "}
-                <br></br>
-                -Bob
-              </p>
-              <span>
-                {" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-              </span>
-            </div>
-            <div className="white">
-              <p className="darkBlueTxt">
-                Cruz fixed my electrical stuff quickly and efficiently!{" "}
-                <br></br>
-                -Bob
-              </p>
-              <span>
-                {" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-                <BsFillStarFill color="#ffcd02" size={15} />{" "}
-              </span>
-            </div>
-          </section>
-     
-          <h4 className="darkBlueTxt">Ask About Our Apprenticeship Program</h4>
+          </div>
+
+          <div className={styles.testimonialsContainer}>
+            <section>
+              <h3 className="darkBlueTxt">Testimonials</h3>
+
+              <div className="white">
+                <p className="darkBlueTxt">
+                  Cruz fixed my electrical stuff quickly and efficiently!{" "}
+                  <br></br>
+                  -Bob
+                </p>
+                <span>
+                  {" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                </span>
+              </div>
+
+              <div className="white">
+                <p className="darkBlueTxt">
+                  Cruz fixed my electrical stuff quickly and efficiently!{" "}
+                  <br></br>
+                  -Bob
+                </p>
+                <span>
+                  {" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                </span>
+              </div>
+              <div className="white">
+                <p className="darkBlueTxt">
+                  Cruz fixed my electrical stuff quickly and efficiently!{" "}
+                  <br></br>
+                  -Bob
+                </p>
+                <span>
+                  {" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                </span>
+              </div>
+              <div className="white">
+                <p className="darkBlueTxt">
+                  Cruz fixed my electrical stuff quickly and efficiently!{" "}
+                  <br></br>
+                  -Bob
+                </p>
+                <span>
+                  {" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                  <BsFillStarFill color="#ffcd02" size={15} />{" "}
+                </span>
+              </div>
+            </section>
+
+            <h4 className="darkBlueTxt">
+              Ask About Our Apprenticeship Program
+            </h4>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
     </>
   );
 }
